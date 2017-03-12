958dfe50c13acbd69622af109066abeeda70d2bb
Shakalee14 <shakalee14@gmail.com>
2017-03-11 18:41:40 -0800

    front end for message board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/post', function(req, res, next){
+  res.render('create-post')
+})
+
+router.get('/community', function(req, res, next) {
+  // db.getMessages()
+  //   .then( function(messages){
+  //     res.render('message-board', {messages:messages});
+  //   })
+
+  res.render('message-board')
+});
+
+router.post('/messages', function(req, res, next){
+  res.redirect('/community')
+})
+
 
 module.exports = router;
