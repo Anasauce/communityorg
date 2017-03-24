978d1940f1f58a0a98e8d3ffdfa0853c28ca939d
anasauce <anasauceda1843@gmail.com>
2017-03-24 10:18:59 -0700

    got second link of old page up with pictures

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,28 @@ var router = express.Router();
 var db = require('../db.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/post', function(req, res, next){
-  res.render('create-post')
+router.get('/', (request, response, next) => {
+  response.render('splash', { title: 'Gay Shame' });
 })
 
-router.get('/community', function(req, res, next) {
+router.get('/index', (request, response, next) => {
+  response.render('index')
+})
+
+router.get('/post', (request, response, next) => {
+  response.render('create-post')
+})
+
+router.get('/community', (request, response, next) => {
   // db.getMessages()
   //   .then( function(messages){
-  //     res.render('message-board', {messages:messages});
+  //     response.render('message-board', {messages:messages});
   //   })
 
-  res.render('message-board')
+  response.render('message-board')
 });
 
-router.post('/messages', function(req, res, next){
+router.post('/messages', function(request, response, next){
   res.redirect('/community')
 })
 
