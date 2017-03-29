aff743654da480b12a49ee30d7f18ff6c7e27b63
Shakalee14 <shakalee14@gmail.com>
2017-03-29 14:46:14 -0700

    Added correct links, home route and custom 404 page

--- app.js
+++ app.js
@@ -34,10 +34,29 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('nofindy', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
 });
 // error handler
 app.use(function(err, req, res, next) {
